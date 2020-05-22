7a6c3271881e85fbde248a53448786b3e032db64
Akshay Upadhayay <akshay.upadhayay@gmail.com>
2020-05-22 18:08:27 +0000

    date function

--- commands/commands.sh
+++ commands/commands.sh
@@ -1,2 +1,13 @@
 FILELIST=`ls -la`
 echo $FILELIST
+
+
+FileWithTimeStamp=/tmp/my-dir/file_$(/bin/date +%Y-%m-%d).txt
+echo $FileWithTimeStamp
+=> /tmp/my-dir/file_2020-05-22.txt
+
+
+BIRTHDATE="Jan 1, 2020"
+BIRTHDAY=`date -d "$BIRTHDATE" +%A`
+echo $BIRTHDAY
+=> Wednesday
