dfdeb1c4d165cb4f683da4b1fe98ac36830bc7c6
Akshay Upadhayay <akshay.upadhayay@gmail.com>
2020-05-22 11:34:25 +0000

    repl changes

--- ShellOne/bashOne.sh
+++ ShellOne/bashOne.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 # bash test
+# Test path: /home/runner/bash/ShellOne
+
 
 echo "Enter version and gituser"
 
 read VERSION USERNAME
 
 echo "Version is $VERSION and username is $USERNAME"
+
+if [ $VERSION -eq 2 ];
+then
+	echo "Correct version"
+else
+	echo "Not correct version"
+fi
+
+
+if [ -e /home/runner/bash/ShellOne/bashOne.sh ];
+then
+	echo "Script found"
+else
+	echo "Script not found"
+fi
+
