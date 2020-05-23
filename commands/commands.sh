52f50f7f4c9e19806d57aae0e166d6ba6eb7599e
Akshay Upadhayay <akshay.upadhayay@gmail.com>
2020-05-23 16:56:05 +0000

    array file 3

--- commands/commands.sh
+++ commands/commands.sh
@@ -210,5 +210,27 @@ newarr=("${arr[@]}" "${arr[@]}" "${arr[@]}")
 echo ${newarr[@]}
 
 
+#array command - Find unique element in array
+# 5           -> Input
+# 0 0 1 2 1
+# =>2
+read
+arr=($(cat))
+arr=${arr[@]}
+echo $((${arr// /^}))
+
+#Explanation
+# arr=($(cat)) -> reads in the array
+# arr=${arr[*]} -> render a new variable of type string from the merging of the array arr delimited by space, i.e., from [1,2,2,2,1] to "1 2 2 2 1"
+# ${arr// /^} -> replaces all spaces ' ' in the string variable with ^ (bitwise-XOR operator),i.e., we get "1^2^2^2^1"
+# $((exp)) -> evaluates the expression "exp" by performing arithmetic operations, here "exp" => ${arr// /^}. Thus ^ is treated as arithmetic operator and the string is evaluated sequentially form left to right.
+# We get, 1^2^2^2^1 = 3^2^2^1 = 1^2^1 = 3^1 = 2 (Lonely Integer)
+
+#OR
+
+read
+echo $((`tr ' ' '^'`))
+
+
 
 
