3d6218bd8c1f243cf5fc825665f69fc033f47f2c
Akshay Upadhayay <akshay.upadhayay@gmail.com>
2020-05-23 03:35:08 +0000

    hackerrank 1

--- commands/commands.sh
+++ commands/commands.sh
@@ -1,10 +1,27 @@
+for f in {1..99..2}
+do
+    echo "$f"
+done
+=>1 3 5 7 9...99
+
+
+read X
+read Y
+echo $(($X+$Y))
+echo $(($X-$Y))
+echo $(($X*$Y))
+echo $(($X/$Y))
+
+
 FILELIST=`ls -la`
 echo $FILELIST
+
 
 
 FileWithTimeStamp=/tmp/my-dir/file_$(/bin/date +%Y-%m-%d).txt
 echo $FileWithTimeStamp
 => /tmp/my-dir/file_2020-05-22.txt
+
 
 
 BIRTHDATE="Jan 1, 2020"
