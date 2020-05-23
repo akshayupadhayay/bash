2cba6363176527bff9f258f60d5cb24b06ab1f07
Akshay Upadhayay <akshay.upadhayay@gmail.com>
2020-05-23 03:52:17 +0000

    commands

--- commands/commands.sh
+++ commands/commands.sh
@@ -1,10 +1,11 @@
+#loops
 for f in {1..99..2}
 do
     echo "$f"
 done
 =>1 3 5 7 9...99
 
-
+#maths
 read X
 read Y
 echo $(($X+$Y))
@@ -12,18 +13,33 @@ echo $(($X-$Y))
 echo $(($X*$Y))
 echo $(($X/$Y))
 
+#comparison
+read X
+read Y
+if [ $X -lt $Y ]
+then
+    echo "X is less than Y"
+elif [ $X -gt $Y ]
+then
+    echo "X is greater than Y"
+elif [ $X -eq $Y ]
+then
+    echo "X is equal to Y"
+fi
 
+
+#assign output to variable
 FILELIST=`ls -la`
 echo $FILELIST
 
 
-
+#create filename runtime
 FileWithTimeStamp=/tmp/my-dir/file_$(/bin/date +%Y-%m-%d).txt
 echo $FileWithTimeStamp
 => /tmp/my-dir/file_2020-05-22.txt
 
 
-
+#extract day from date
 BIRTHDATE="Jan 1, 2020"
 BIRTHDAY=`date -d "$BIRTHDATE" +%A`
 echo $BIRTHDAY
